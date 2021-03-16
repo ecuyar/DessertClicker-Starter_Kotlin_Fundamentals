0ab04672385d5b3719c1fc19aa1ac0f535a03011
Enes Can UYAR <uyar.enescan@gmail.com>
2021-03-16 09:20:42 +0300

    Reduce on click number

--- app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -47,17 +47,17 @@ class MainActivity : AppCompatActivity() {
     private val allDesserts = listOf(
             Dessert(R.drawable.cupcake, 5, 0),
             Dessert(R.drawable.donut, 10, 5),
-            Dessert(R.drawable.eclair, 15, 20),
-            Dessert(R.drawable.froyo, 30, 50),
-            Dessert(R.drawable.gingerbread, 50, 100),
-            Dessert(R.drawable.honeycomb, 100, 200),
-            Dessert(R.drawable.icecreamsandwich, 500, 500),
-            Dessert(R.drawable.jellybean, 1000, 1000),
-            Dessert(R.drawable.kitkat, 2000, 2000),
-            Dessert(R.drawable.lollipop, 3000, 4000),
-            Dessert(R.drawable.marshmallow, 4000, 8000),
-            Dessert(R.drawable.nougat, 5000, 16000),
-            Dessert(R.drawable.oreo, 6000, 20000)
+            Dessert(R.drawable.eclair, 15, 10),
+            Dessert(R.drawable.froyo, 30, 15),
+            Dessert(R.drawable.gingerbread, 50, 20),
+            Dessert(R.drawable.honeycomb, 100, 25),
+            Dessert(R.drawable.icecreamsandwich, 500, 27),
+            Dessert(R.drawable.jellybean, 1000, 30),
+            Dessert(R.drawable.kitkat, 2000, 33),
+            Dessert(R.drawable.lollipop, 3000, 35),
+            Dessert(R.drawable.marshmallow, 4000, 38),
+            Dessert(R.drawable.nougat, 5000, 40),
+            Dessert(R.drawable.oreo, 6000, 42)
     )
     private var currentDessert = allDesserts[0]
 
